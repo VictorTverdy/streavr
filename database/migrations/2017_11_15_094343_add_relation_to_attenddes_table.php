aa135c8e9fc79f98564f5dd3c1283aec19f681fe
Victor Tverdy <victor.tverdy.w@gmail.com>
2017-11-15 11:56:19 +0200

    Add relation from atteendees to user table

---
+++ database/migrations/2017_11_15_094343_add_relation_to_attenddes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRelationToAttenddesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::enableForeignKeyConstraints();
+        Schema::table('attendees', function (Blueprint $table) {
+            //
+            $table->integer('user_id', false,true)->change();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::enableForeignKeyConstraints();
+        Schema::table('attendees', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+        });
+    }
+}
